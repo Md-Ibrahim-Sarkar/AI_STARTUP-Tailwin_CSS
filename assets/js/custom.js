3c19ebc0d7df04fc3f1cbbc356a10e4da88ae272
ibrahim06978 <ibrahim06978@gmail.com>
2024-10-11 10:56:49 +0600

    done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,3 +11,27 @@ cancelIcon.addEventListener('click', () => {
   let toggleBar = document.querySelector('.toggle-bar');
   toggleBar.classList.add('hidden');
 });
+
+
+//  *~~~~~~~~~~~~~~~ Scroll Reveal Animation ~~~~~~~~~~~~~~~~*
+
+let animation = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 500,
+  reset: true
+})
+
+animation.reveal('.animation-one');
+animation.reveal('.animation-two', {
+  delay: 500,
+  scale: 0.05,
+  duration: 1000
+});
+animation.reveal('.animation-three', {
+  origin: 'left',
+  distance: '60px',
+});
+
+animation.reveal('.animation-four');
